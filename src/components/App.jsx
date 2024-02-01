cffa2400708afaeb204b1d0b3e805be7ec3a79ce
Dean <dean.w1993@gmail.com>
2024-02-01 11:24:29 +0000

    setup: add project file structure
    add navigation bar

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+// import "./App.css";
+import NavigationBar from "./NavigationBar";
+
+function App() {
+  return (
+    <>
+      <NavigationBar />
+      <h1>hello word</h1>
+      {/* //TODO add in search bar */}
+      {/* TODO add some product to page */}
+    </>
+  );
+}
+
+export default App;
