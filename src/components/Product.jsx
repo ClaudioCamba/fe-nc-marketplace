790bbc3b14c4cf77f0639f27443fcf82de5baa5f
ClaudioCamba <33432769+ClaudioCamba@users.noreply.github.com>
2024-02-02 08:39:29 +0000

    update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,23 +1,16 @@
-import Badge from 'react-bootstrap/Badge';
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
 
 function Product({title, price, imgUrl}) {
     return (
     <>
-            
-    <div className="product-item ms-2 me-auto">
     <Image src={imgUrl} fluid thumbnail/>
     <div className='product-detail'>
-        <div className="fw-bold">{title}</div>
-            <p>£{price}</p>
-            <Button variant="primary">Buy Now</Button>{' '}
-        </div>
+    <div className="fw-bold">{title}</div>
+        <p>£{price}</p>
+        <Button variant="primary">Buy Now</Button>{' '}
     </div>
-    {/* <Badge bg="primary" pill>14</Badge> */}
-        
     </>
-
     )
 }
 
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -10,16 +10,16 @@ function ProductList() {
   const [allItems, setAllItems] = useState([]);
   useEffect(()=>{
     getItems()
-    .then((data)=>setAllItems(data.items))
+    .then((data)=> setAllItems(data.items))
     console.log('test')
-  },[allItems])
+  },[])
  
   return (
-    <ListGroup as="ol" numbered>
+    <ListGroup as="ul">
         {allItems.map((item)=>{
-        return <ListGroup.Item key={item.item_id} as="li" className="d-flex justify-content-between align-items-start">
+        return <ListGroup.Item key={item.item_id} as="li" className="product-item">
                 <Product title={item.item_name} price={item.price} imgUrl={item.img_url} />
-            </ListGroup.Item>
+              </ListGroup.Item>
         })}
     </ListGroup>
   );
